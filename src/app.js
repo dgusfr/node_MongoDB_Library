0a4c47d2e1048d31216a5e75b1bf5abfc706dc62
dgusfr <diegogustavo.fr@gmail.com>
2024-12-11 08:35:19 -0300

    feat(update): app with poo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import "./config/database.js";
-import {
-  listarLivros,
-  obterLivroPorId,
-  adicionarLivro,
-  atualizarLivro,
-  deletarLivro,
-} from "./controllers/livroController.js";
+import LivroController from "./controllers/LivroController.js";
 
 dotenv.config();
 
@@ -19,10 +13,11 @@ app.get("/", (req, res) => {
   res.status(200).send("API de Livraria");
 });
 
-app.get("/livros", listarLivros);
-app.get("/livros/:id", obterLivroPorId);
-app.post("/livros", adicionarLivro);
-app.put("/livros/:id", atualizarLivro);
-app.delete("/livros/:id", deletarLivro);
+// Rotas de livros
+app.get("/livros", (req, res) => LivroController.listar(req, res));
+app.get("/livros/:id", (req, res) => LivroController.obterPorId(req, res));
+app.post("/livros", (req, res) => LivroController.adicionar(req, res));
+app.put("/livros/:id", (req, res) => LivroController.atualizar(req, res));
+app.delete("/livros/:id", (req, res) => LivroController.deletar(req, res));
 
 export default app;
