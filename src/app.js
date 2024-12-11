26fd5d6f530bdfc086c9a7b8cefb8f3dbf2009be
dgusfr <diegogustavo.fr@gmail.com>
2024-12-11 08:43:39 -0300

    feat(update): add cors and helmet

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
 import "./config/database.js";
 import LivroController from "./controllers/LivroController.js";
 
@@ -7,13 +9,18 @@ dotenv.config();
 
 const app = express();
 
+// Middleware de segurança
+app.use(helmet());
+app.use(cors());
+
+// Middleware para JSON
 app.use(express.json());
 
+// Rotas da API
 app.get("/", (req, res) => {
   res.status(200).send("API de Livraria");
 });
 
-// Rotas de livros
 app.get("/livros", (req, res) => LivroController.listar(req, res));
 app.get("/livros/:id", (req, res) => LivroController.obterPorId(req, res));
 app.post("/livros", (req, res) => LivroController.adicionar(req, res));
