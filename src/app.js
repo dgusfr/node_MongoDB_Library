bf8bc1fc0d5d6f5a6d21d754028a4e77be9a5cba
dgusfr <diegogustavo.fr@gmail.com>
2024-12-10 22:17:16 -0300

    feat(add): new routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import "./config/database.js";
-import livroRoutes from "./routes/livros.js";
+import {
+  listarLivros,
+  obterLivroPorId,
+  adicionarLivro,
+  atualizarLivro,
+  deletarLivro,
+} from "./controllers/livroController.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use("/livros", livroRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("API de Livraria");
 });
+
+app.get("/livros", listarLivros);
+app.get("/livros/:id", obterLivroPorId);
+app.post("/livros", adicionarLivro);
+app.put("/livros/:id", atualizarLivro);
+app.delete("/livros/:id", deletarLivro);
 
 export default app;
