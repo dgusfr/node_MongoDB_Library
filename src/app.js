164b7701f571eeb2f58828e7eda2d29b504f53b3
dgusfr <diegogustavo.fr@gmail.com>
2024-12-11 09:04:15 -0300

    fix(update): aopp

--- src/app.js
+++ src/app.js
@@ -4,27 +4,28 @@ import helmet from "helmet";
 import cors from "cors";
 import "./config/database.js";
 import LivroController from "./controllers/LivroController.js";
+import autorRoutes from "./routes/autorRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-// Middleware de segurança
 app.use(helmet());
 app.use(cors());
-
-// Middleware para JSON
 app.use(express.json());
 
-// Rotas da API
 app.get("/", (req, res) => {
   res.status(200).send("API de Livraria");
 });
 
+// Rotas de Livros
 app.get("/livros", (req, res) => LivroController.listar(req, res));
 app.get("/livros/:id", (req, res) => LivroController.obterPorId(req, res));
 app.post("/livros", (req, res) => LivroController.adicionar(req, res));
 app.put("/livros/:id", (req, res) => LivroController.atualizar(req, res));
 app.delete("/livros/:id", (req, res) => LivroController.deletar(req, res));
+
+// Rotas de Autores
+app.use("/autores", autorRoutes);
 
 export default app;
