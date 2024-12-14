ba23e245fd11e30f47fbcbfc4f532b60deac58e6
Diego Franco <105020827+dgusfr@users.noreply.github.com>
2024-12-14 18:53:35 -0300

    Update Livro.js

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
 
-const LivroSchema = new mongoose.Schema(
-  {
-    id: { type: Number, required: true, unique: true },
-    titulo: { type: String, required: true },
-    autorId: { type: Number, required: true },
-    preco: { type: Number, required: true },
-    anoPublicacao: { type: Number },
-  },
-  {
-    versionKey: false,
-  }
-);
+const LivroSchema = new mongoose.Schema({
+  titulo: { type: String, required: true },
+  autor: { type: mongoose.Schema.Types.ObjectId, ref: "Autor", required: true },
+  editora: { type: String, required: true },
+  preco: { type: Number, required: true, min: 0 },
+  anoPublicacao: { type: Number, required: true, min: 1500 },
+}, {
+  versionKey: false,
+});
 
 const Livro = mongoose.model("Livro", LivroSchema);
 
